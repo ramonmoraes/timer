c27a5a5dcc501995b2dd38e4261a5648408c3a48
Ramon Moraes - Dart <ramon.moraes94@gmail.com>
2020-03-19 11:37:55 -0300

    initial main

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	playBeep(2)
+}
+
+func playBeep(seconds time.Duration) {
+	f, err := os.Open("./audio/sample.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer streamer.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	speaker.Play(streamer)
+	time.Sleep(time.Second * seconds)
 }
