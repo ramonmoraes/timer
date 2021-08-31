d9e15d3eca706bc44d2094f4e5888957f46547c1
Ramon Moraes <ramon.moraes94@gmail.com>
2021-08-31 15:44:51 -0300

    Using go 1.16 embed module (#4)
    * handling signals
    
    * better commentary
    
    * update go version
    
    * update go sum
    
    * Using new embed package
    
    * remove unecessary code
    
    * remove go generate command
    
    * remove depracated ioutil

--- audio/loader.go
+++ audio/loader.go
@@ -2,20 +2,13 @@ package audio
 
 import (
 	"bytes"
+	_ "embed"
 	"io"
-	"io/ioutil"
-	"log"
-	"os"
 )
 
-func LoadRaw() io.ReadCloser {
-	f, err := os.Open("./audio/audio.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return f
-}
+//go:embed audio.mp3
+var AudioBytes []byte
 
 func LoadSerialized() io.ReadCloser {
-	return ioutil.NopCloser(bytes.NewReader(AudioBytes))
+	return io.NopCloser(bytes.NewReader(AudioBytes))
 }
